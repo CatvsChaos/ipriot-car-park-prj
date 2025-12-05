95223f7162edb4a1af1964981c0ef0f002efe53b
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-05 20:22:47 +0800

    feat: add register method to class CarPark, sensors to parameters, and import from Sensor and Display

--- src/car_park.py
+++ src/car_park.py
@@ -1,11 +1,27 @@
+from sensor import Sensor
+from display import Display
+
 class CarPark:
-    def __init__(self, location, capacity, plates, displays):
+    def __init__(self, location, capacity, plates, sensors, displays):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
+        self.sensors = sensors or []
         self.displays = displays or []
 
     def __str__(self):
         return f"Car park location : {self.location} \n Capacity : {self.capacity}"
+
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or Display")
+
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
+
+
+
 
 
