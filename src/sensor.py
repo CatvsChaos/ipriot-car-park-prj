bff7b31551996afc1bc2b91e3bb883aa29acb59b
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-05 15:36:22 +0800

    feat: add __init__ and __str__ methods to class Sensor with parameters

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,8 @@
 class Sensor:
-    pass
+    def __init__(self, id, is_active, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+    def __str__(self):
+        return f"Sensor {self.id}: {self.is_active}"
