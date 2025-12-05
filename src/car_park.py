ec7db3f291773462d16fcda4967a68f5bd469d56
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-05 15:11:05 +0800

    feat: add __init__ to class CarPark with parameters

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,7 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates, displays):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.displays = displays or []
+
