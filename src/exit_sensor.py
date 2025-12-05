330aba09562df0852d1aef3487f22a9dcb00c3fc
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-05 22:50:02 +0800

    feat: add update_car_park and _scan_plate methods to class ExitSensor

--- src/exit_sensor.py
+++ src/exit_sensor.py
@@ -1,3 +1,15 @@
+import random
+
+
 class ExitSensor:
     def __init__(self):
-        pass
+        self.car_park = None
+    
+    def update_car_park(self, plate):
+        self.car_park.remove_car(plate)
+        print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
+
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
+
+    
