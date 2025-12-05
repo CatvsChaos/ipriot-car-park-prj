fee9ff9fc5d84b93e05d59bc3cc2a43cec51fa8a
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-05 15:28:24 +0800

    feat: add __init__ and __str__ methods to class Display with parameters

--- src/display.py
+++ src/display.py
@@ -1,2 +1,8 @@
 class Display:
-    pass
+    def __init__(self, id, message, is_on):
+        self.id = id
+        self.message = ""
+        self.is_on = False
+
+    def __str__(self):
+        return f"Display {self.id}: Welcome to the car park."
