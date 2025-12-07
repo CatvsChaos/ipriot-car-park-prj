328a8de6d63a1624698a38bc8e853381f832ea07
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-07 22:22:15 +0800

    unittest: fix all errors in test_sensor.py

--- tests/test_sensor.py
+++ tests/test_sensor.py
@@ -1,30 +1,25 @@
 import unittest
-from sensor import Sensor
-from entry_sensor import EntrySensor
-from exit_sensor import ExitSensor
-from car_park import CarPark
+import car_park
+import sensor
 
 class TestSensor(unittest.TestCase):
     def setUp(self):
-        self.sensor = Sensor(1, False, CarPark("123 Example Street", 100, [], []))
+        self.car_park = car_park.CarPark("123 Example Street", 100, [], [], [])
+        self.entry_sensor = sensor.EntrySensor(1, False, self.car_park)
+        self.exit_sensor = sensor.ExitSensor(1, True, self.car_park)
 
-    def test_sensor_initialized_with_all_attributes(self):
-        self.assertEqual(self.sensor.id, 1)
-        self.assertEqual(self.Sensor.is_active, False)
-        # self.assertEqual(self.sensor.car_park, CarPark( "123 Example Street", 100, [], []))
-        self.assertEqual(self.sensor.car_park, car_park)
+    def test_entry_sensor_initialized_with_all_attributes(self):
+        self.assertIsInstance(self.entry_sensor, sensor.Sensor)
+        self.assertEqual(self.entry_sensor.id, 1)
+        self.assertEqual(self.entry_sensor.is_active, False)
+        self.assertEqual(self.entry_sensor.car_park, self.car_park)
+
+    def test_exit_sensor_initialized_with_all_attributes(self):
+        self.assertIsInstance(self.exit_sensor, sensor.Sensor)
+        self.assertEqual(self.exit_sensor.id, 1)
+        self.assertEqual(self.exit_sensor.is_active, True)
+        self.assertEqual(self.exit_sensor.car_park, self.car_park)
 
     def test_detect_vehicle(self):
-        self.assertEqual(self.sensor.entry_sensor.plate, "FAKE-001")
-        self.assertEqual(self.sensor.entry_sensor)
-
-
-    # class Sensor(ABC):
-    #     def __init__(self, id, is_active, car_park):
-    #         self.id = id
-    #         self.is_active = is_active
-    #         self.car_park = car_park
-
-        # def detect_vehicle(self):
-        #     plate = self._scan_plate()
-        #     self.update_car_park(plate)
+        self.entry_sensor.detect_vehicle()
+        self.exit_sensor.detect_vehicle()
