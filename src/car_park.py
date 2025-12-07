119884cac2369723b0f188f00e57eec8d126dfbc
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-08 02:11:30 +0800

    feat: add parameter to CarPark __init__ to create log file

--- src/car_park.py
+++ src/car_park.py
@@ -1,13 +1,18 @@
 from sensor import Sensor
 from display import Display
+from pathlib import Path
+from datetime import datetime
+
 
 class CarPark:
-    def __init__(self, location, capacity, plates, sensors, displays):
+    def __init__(self, location, capacity, plates, sensors, displays, log_file=Path("log.txt")):
         self.location = location
         self.capacity = capacity
         self.plates = plates or []
         self.sensors = sensors or []
         self.displays = displays or []
+        self.log_file = log_file if isinstance(log_file, Path) else Path(log_file)
+        self.log_file.touch(exist_ok=True)
 
     def __str__(self):
         return f"Car park location : {self.location} \n Capacity : {self.capacity}"
@@ -36,6 +41,7 @@ class CarPark:
         data = {"available bays": self.available_bays, "temperature": 25}
         for display in self.displays:
             display.update(data)
+
 
 
 
