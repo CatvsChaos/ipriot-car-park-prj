7d19a4760ea633d0b0f6d81d3c4996afd8fb9457
Jacqueline Bout <Jacqueline.Bout@mobility.com.au>
2025-12-07 22:09:57 +0800

    refactor: change order of methods in class Sensor and add comments to scan plate methods

--- src/sensor.py
+++ src/sensor.py
@@ -1,6 +1,7 @@
 import random
 from abc import ABC, abstractmethod
 
+
 class Sensor(ABC):
     def __init__(self, id, is_active, car_park):
         self.id = id
@@ -10,10 +11,7 @@ class Sensor(ABC):
     def __str__(self):
         return f"Sensor {self.id}: {self.is_active}"
 
-    @abstractmethod
-    def update_car_park(self, plate):
-        pass
-
+    # Simulate a car entering the car park by randomly generating a new plate
     def _scan_plate(self):
         return 'FAKE-' + format(random.randint(0,999), '03d')
 
@@ -21,19 +19,25 @@ class Sensor(ABC):
         plate = self._scan_plate()
         self.update_car_park(plate)
 
+    @abstractmethod
+    def update_car_park(self, plate):
+        pass
+
+# =========================================
 
 class EntrySensor(Sensor):
-
     def update_car_park(self, plate):
         self.car_park.add_car(plate)
         print(f"Incoming 🚘 vehicle detected. Plate: {plate}")
 
 class ExitSensor(Sensor):
+    # Simulate a car leaving the car park by randomly selecting an existing plate
+    def _scan_plate(self):
+        return random.choice(self.car_park.plates)
 
     def update_car_park(self, plate):
         self.car_park.remove_car(plate)
         print(f"Outgoing 🚗 vehicle detected. Plate: {plate}")
 
-    def _scan_plate(self):
-        return random.choice(self.car_park.plates)
+
 
